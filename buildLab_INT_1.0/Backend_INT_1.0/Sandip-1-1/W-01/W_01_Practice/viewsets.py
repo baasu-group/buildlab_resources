0fbcf4dad2e5a3995bae70a715e9f28f34c05bcf
Sandip-1-1 <sandipsapkota001@gmail.com>
2026-08-16 17:41:11 +0545

    api_views.py removed and viewsets and router added

---
+++ buildLab_INT_1.0/Backend_INT_1.0/Sandip-1-1/W-01/W_01_Practice/viewsets.py
@@ -0,0 +1,35 @@
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Todo
+from .serializers import TodoSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+    serializer_class=TodoSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Todo.objects.filter(
+            user=self.request.user
+        ).order_by('-created_at')
+
+    def perform_create(self,serializer):
+        serializer.save(user=self.request.user)
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return [permissions.IsAuthenticated()]
+        return [permissions.IsAuthenticated()]
+
+    @action(detail=False,methods=['get'])
+    def completed(self,request):
+        todos=self.get_queryset().filter(completed=True)
+        serializer= self.get_serializer(todos,many=True)
+        return Response(serializer.data)
+
+    @action(detail=False ,methods=['get'])
+    def pending(self,request):
+        todos=self.get_queryset().filter(completed=False)
+        serializer= self.get_serializer(todos,many=True)
+        return Response(serializer.data)
+
--- buildLab_INT_1.0/Backend_INT_1.0/Sandip-1-1/W-01/Week_01/urls.py
+++ buildLab_INT_1.0/Backend_INT_1.0/Sandip-1-1/W-01/Week_01/urls.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
-from django.urls import path
-from W_01_Practice import api_views
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from W_01_Practice import viewsets
 from rest_framework_simplejwt.views import(
     TokenObtainPairView,
     TokenRefreshView
 )
 
+router=DefaultRouter()
+router.register('todos',viewsets.TodoViewSet,basename='todo')
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/todos/',api_views.api_todo_list,name='api_todo_list'),
-    path('api/todos/create/',api_views.api_todo_create,name='api_todo_create'),
-    path('api/todos/<int:id>/update/',api_views.api_todo_update,name='api_todo_update'),
-    path('api/todos/<int:id>/delete/',api_views.api_todo_delete,name='api_todo_delete'),
+    path('api/',include(router.urls)),
     path('api/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
     path('api/token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
 ]
