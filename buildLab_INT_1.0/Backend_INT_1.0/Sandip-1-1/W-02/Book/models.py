e8071ab46f6c4e859366f9571d486f5e1f49193a
Sandip-1-1 <sandipsapkota001@gmail.com>
2026-08-20 15:06:31 +0545

    Model created

--- buildLab_INT_1.0/Backend_INT_1.0/Sandip-1-1/W-02/Book/models.py
+++ buildLab_INT_1.0/Backend_INT_1.0/Sandip-1-1/W-02/Book/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Book(models.Model):
+    title=models.CharField(max_length=200)
+    author=models.CharField
+    genre=models.CharField
+    published_year=models.IntegerField
+    user=models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+
+    def __str__(self):
+        return self.title
+
