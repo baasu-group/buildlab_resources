ff8a9da557910e455909acb1d3ebbb294011cde3
Sandip-1-1 <sandipsapkota001@gmail.com>
2026-08-26 11:35:05 +0545

    urls fixed

--- buildLab_INT_1.0/Backend_INT_1.0/Sandip-1-1/W-02/Student/urls.py
+++ buildLab_INT_1.0/Backend_INT_1.0/Sandip-1-1/W-02/Student/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from . import viewsets
 
 router = DefaultRouter()
-router.register('students', viewsets.StudentViewSet, basename='book')
+router.register('students', viewsets.StudentViewSet, basename='student')
 router.register('grades',viewsets.GradeViewSet,basename='grade')
 urlpatterns = router.urls
